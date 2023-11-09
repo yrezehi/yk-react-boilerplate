d497a9719ea76a794994d80fcc9ba9dbb045487a
unknown <yrezehi@gmail.com>
2023-11-08 21:27:00 -0800

    finish: cra to vite

--- ykreactboilerplate/vite.config.js
+++ ykreactboilerplate/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
+    define: {
+      'process.env': {},
+    },
     build: {
       outDir: 'build',
     },
